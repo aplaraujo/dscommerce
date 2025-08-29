3a7805770b2e68964996ef6d01e026f95a3d1afb
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-08-29 14:44:26 -0300

    GET products paged

--- src/main/java/com/example/dscommerce/services/ProductService.java
+++ src/main/java/com/example/dscommerce/services/ProductService.java
@@ -3,6 +3,8 @@ package com.example.dscommerce.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,5 +24,11 @@ public class ProductService {
         Product product = result.get();
         ProductDTO dto = new ProductDTO(product);
         return dto;
+    }
+
+    @Transactional(readOnly=true)
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> products = productRespository.findAll(pageable);
+        return products.map(prod -> new ProductDTO(prod));
     }
 }
