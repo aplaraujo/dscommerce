fd4673181b930c896e227ecaf74019cc7c678590
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-08-31 09:56:55 -0300

    Customized exception part 1

--- src/main/java/com/example/dscommerce/services/ProductService.java
+++ src/main/java/com/example/dscommerce/services/ProductService.java
@@ -11,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.example.dscommerce.dto.ProductDTO;
 import com.example.dscommerce.entities.Product;
 import com.example.dscommerce.repositories.ProductRepository;
+import com.example.dscommerce.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ProductService {
@@ -21,7 +22,7 @@ public class ProductService {
     @Transactional(readOnly=true)
     public ProductDTO findById(Long id) {
         Optional<Product> result = productRepository.findById(id);
-        Product product = result.get();
+        Product product = result.orElseThrow(() -> new ResourceNotFoundException("Resource not found!"));
         ProductDTO dto = new ProductDTO(product);
         return dto;
     }
