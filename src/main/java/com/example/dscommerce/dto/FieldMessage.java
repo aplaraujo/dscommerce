e861975d02da09eb647644ef6db7aa5c62af1100
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-12-01 19:59:21 -0300

    Entities and DTO refactored part 2

--- src/main/java/com/example/dscommerce/dto/FieldMessage.java
+++ src/main/java/com/example/dscommerce/dto/FieldMessage.java
@@ -1,20 +1,6 @@
 package com.example.dscommerce.dto;
 
-public class FieldMessage {
-
-    private final String fieldName;
-    private final String message;
-    
-    public FieldMessage(String fieldName, String message) {
-        this.fieldName = fieldName;
-        this.message = message;
-    }
-
-    public String getFieldName() {
-        return fieldName;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-}
+public record FieldMessage(
+        String fieldName,
+        String message
+) {}
